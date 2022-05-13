9f09af94c9a71e9b143d081e1c720ca7b9c0276c
L2237 <86204143+L2237@users.noreply.github.com>
2022-05-13 13:12:29 +0300

    Update User.java

--- src/main/java/com/springboot/app/entities/User.java
+++ src/main/java/com/springboot/app/entities/User.java
@@ -3,11 +3,26 @@ package com.springboot.app.entities;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table(name = "user")
 @RequiredArgsConstructor
 public class User {
+    @Id
+    @Column(name = "user_Id")
     private String userId;
+
+    @Column(name = "user_Name")
     private String userName;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
 }
