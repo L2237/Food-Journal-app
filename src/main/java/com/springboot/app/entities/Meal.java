a51201565d8c92741dd35fd0a5c9e1f06b7697ae
L2237 <86204143+L2237@users.noreply.github.com>
2022-05-13 13:11:16 +0300

    Update Meal.java

--- src/main/java/com/springboot/app/entities/Meal.java
+++ src/main/java/com/springboot/app/entities/Meal.java
@@ -3,12 +3,25 @@ package com.springboot.app.entities;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Data
 @RequiredArgsConstructor
+@Entity
+@Table(name = "meal")
 public class Meal {
+
+    @Id
+    @Column(name = "meal_Id")
     private String mealId;
+
+    @Column(name = "date")
     private Date date;
+
+    @Column(name = "user_Id")
     private String userId;
 }
